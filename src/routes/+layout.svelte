a71604d9aeea5f6591d4c8054fe157f9ce558f16
danielrudolph <36728226+danielrudolph@users.noreply.github.com>
2023-08-12 01:40:27 +0200

    modified landing page
    Co-authored-by: 1c3fr34k <1c3fr34k@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { LightSwitch } from '@skeletonlabs/skeleton';
 	// The ordering of these imports is critical to your app working properly
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
@@ -14,33 +15,34 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl uppercase">Tech-Howto 🧑‍💻</strong>
+				<a
+					class="ml-4 btn btn-sm variant-ghost-surface hover:bg-violet-400 transition-transform hover:scale-110 hover:shadow-lg hover:rotate-6"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Beiträge
+				</a>
+				<a
+					class="ml-2 btn btn-sm variant-ghost-surface hover:bg-violet-400 transition-transform hover:scale-110 hover:shadow-lg hover:rotate-6"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Tools
+				</a>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
+				<!-- <a
 					class="btn btn-sm variant-ghost-surface"
 					href="https://github.com/skeletonlabs/skeleton"
 					target="_blank"
 					rel="noreferrer"
 				>
 					GitHub
-				</a>
+				</a> -->
+				<LightSwitch />
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
