d91c64fd96a7c0034db6cdbc7968d6a80313f749
danielrudolph <36728226+danielrudolph@users.noreply.github.com>
2023-08-13 00:43:54 +0200

    added route for single post
    Co-authored-by: 1c3fr34k <1c3fr34k@users.noreply.github.com>

---
+++ src/routes/posts/[id]/+page.svelte
@@ -0,0 +1,12 @@
+
+<script>
+    export let data;
+
+    onMount(async () => {
+		const pb = new PocketBase('http://25.65.20.210:8090');
+        const record = await pb.collection('posts').getOne(data.id);
+		console.log(record);
+	});
+</script>
+
+<h1>Test {data.id}</h1>
